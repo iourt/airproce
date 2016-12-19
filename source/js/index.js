bfe1d63ec73e80d52fcfc7d1ba020cb4b1ca16dc
bear <bear@ajmide.com>
2016-12-19 16:55:28 +0800

    modify

---
+++ source/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+	$('#slides').slidesjs({
+		width: 750,
+		height: 360,
+		play: {
+			active: true,
+			auto: true,
+			interval: 4000,
+			swap: true,
+			pauseOnHover: true,
+			restartDelay: 2500
+		}
+	});
+});
